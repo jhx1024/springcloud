e1abba4ae0ec38528a1f24ce77e24eb96206b632
jhx1024 <jhx13145200@163.com>
2021-03-25 17:53:02 +0800

    github6

--- cloud-alibaba-config-nacos-client3377/src/main/java/com/zl/springcloud/NacosConfigMain3377.java
+++ cloud-alibaba-config-nacos-client3377/src/main/java/com/zl/springcloud/NacosConfigMain3377.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient //通过 Spring Cloud 原生注解 @EnableDiscoveryClient 开启服务注册发现功能
 public class NacosConfigMain3377 {
-
     public static void main(String[] args) {
         SpringApplication.run(NacosConfigMain3377.class,args);
     }
